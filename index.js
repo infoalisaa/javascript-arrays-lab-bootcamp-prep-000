dc5ff895bb9d92b1fe3ed6e7ca2d49f387362ad2
Alisa Arnold <willowtreestar@protonmail.com>
2018-06-08 10:39:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var kittens = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendKitten(name){
   kittens.push(name);
 }
+
 kittens = ['Milo', 'Otis', 'Garfield']
 function destructivelyPrependKitten(name){
   kittens.unshift(name);
 }
+
 kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveLastKitten(){
+  
+}
 kittens = ['Milo', 'Otis', 'Garfield']
 kittens = ['Milo', 'Otis', 'Garfield']
 kittens = ['Milo', 'Otis', 'Garfield']
