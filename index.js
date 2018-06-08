8f56430032cf387d502052a00ffe05cf152cdba4
Alisa Arnold <willowtreestar@protonmail.com>
2018-06-08 10:35:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = "I don't do much."
 kittens = ['Milo', 'Otis', 'Garfield']
-
+function destructivelyAppendKitten(name){
+  
+}
 kittens = ['Milo', 'Otis', 'Garfield']
 kittens = ['Milo', 'Otis', 'Garfield']
 kittens = ['Milo', 'Otis', 'Garfield']
